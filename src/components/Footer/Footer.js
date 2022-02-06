a067e5d5c005787921e242755716297fc5c4dc96
Wonjun Jang <wonwonjun@gmail.com>
2022-02-06 22:10:36 +0900

    Create footer style

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { mainStyle } from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={mainStyle}>
+      <span>© WonjunJang. All rights reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,8 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-  pageLayout,
-} from "./layout.module.css";
+import { heading, pageLayout } from "./layout.module.css";
 import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
 
 const Layout = ({ pageTitle, children, home }) => {
   const data = useStaticQuery(graphql`
@@ -23,19 +16,17 @@ const Layout = ({ pageTitle, children, home }) => {
   `);
 
   return (
-    <div className={pageLayout}>
+    <div>
       <title>
         {pageTitle} | {data.site.siteMetadata.title}
       </title>
       <NavBar />
-      <main>
+      <main className={pageLayout}>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
 
-      <footer>
-        <div></div>
-      </footer>
+      <Footer />
     </div>
   );
 };
