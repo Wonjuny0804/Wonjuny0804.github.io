20bedd414cd60ebc17d7636f98514a6f25b5b879
Wonjun Jang <wonwonjun@gmail.com>
2022-02-07 22:16:38 +0900

    Add github link and button to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { mainStyle } from "./Footer.module.css";
+import { mainStyle, logoButton, rightItems } from "./Footer.module.css";
+import Github from "../../assets/GitHub-Mark-120px-plus.png";
 
 const Footer = () => {
   return (
     <footer className={mainStyle}>
       <span>© WonjunJang. All rights reserved.</span>
+      <div className={rightItems}>
+        <a href={"https://github.com/Wonjuny0804"} target="_blank">
+          <img src={Github} alt="github image" className={logoButton} />
+        </a>
+      </div>
     </footer>
   );
 };
